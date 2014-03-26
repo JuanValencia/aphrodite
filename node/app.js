82b2b131cf1922939f3e7424744ffa3088e22b20
Seungjoon Lee <joonlee0228@gmail.com>
2014-03-26 13:39:38 -0700

    remove the code

--- node/app.js
+++ node/app.js
@@ -14,15 +14,6 @@ app.use(app.router);
 
 var projectName = "Aphrodite";
 
-// MySQL Configuration
-var client = mysql.createConnection({
-	user: 'user',
-	password: 'password',
-	host: 'host',
-	port: '3306',
-	database: 'db'
-});
-
 /**
  * Router
  */
@@ -35,77 +26,6 @@ app.get('/version', function (request, response) {
 app.get('/area/:radius', function (request, response) { 
 	var radius = request.param('radius');
 	response.send("Area is " + example.circleArea(radius));
-});
-
-app.get('/user/:id', function (request, response) { 
-	// Get Parameter and shows
-	response.send("User : " + request.param('id'));
-});
-
-// Post
-app.post('/user', function (request, response) { 
-	// Get Parameters
-	var login = request.param('login');
-	var password = request.param('password');
-	
-	// Do the work
-	
-	response.send("SUCESS");
-});
-
-app.put('/user/:id', function (request, response) { });
-app.del('/user/:id', function (request, response) { });
-
-//**************************MYSQL********************************
-
-// Fetch the records fromMySQL
-app.get('/', function (request, response) { 
-	fs.readFile('./view/list.html', 'utf8', function (error, data) {
-		client.query('SELECT * FROM products', function( error, results) {
-			response.send(ejs.render(data, {
-				data: results
-			}));
-		});
-	});
-});
-
-// Insert Template
-app.get('/insert', function (request, response) {
-	fs.readFile('./view/insert.html', 'utf8', function(error, data) {
-		response.send(data);
-	});
-});
-
-// Edit Template
-app.get('/edit/:id', function (request, response) {
-	fs.readFile('./view/edit.html', 'utf8', function (error, data) {
-		client.query('SELECT * FROM products WHERE id = ?', [request.param('id')], function(error, result) {
-			response.send(ejs.render(data, {data: result[0]}));
-		});
-	});
-});
-
-// Delete the record from MySQL
-app.get('/delete/:id', function (request, response) {
-	client.query('DELETE FROM products WHERE id=?', [request.param('id')], function() {
-		response.redirect('/');
-	});
-});
-
-// Insert the record from MySQL
-app.post('/insert', function (request, response) {
-	var body = request.body;	
-	client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [body.name, body.modelnumber, body.series], function() {
-	response.redirect('/');
-	});
-});
-
-// Edit the record from MySQL  
-app.post('/edit/:id', function (request, response) {
-	var body = request.body;
-	client.query('UPDATE products SET name=?, modelnumber=?, series=? WHERE id=?', [body.name, body.modelnumber, body.series, request.param('id')], function () {
-		response.redirect('/');
-	});
 });
 
 // Server Start
